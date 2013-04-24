90bb822012a4cb3fbf60c766530f98dae0459c41
Daniel Leung <daniel.leung@intel.com>
2013-04-24 10:17:55 -0700

    Add package attributes for release key signing
    Packages attribites are added so that sign_target_files_apks
    under build/tools/releasetools can use these attributes to
    re-sign the binaries.
    
    For: AXIA-2387
    Change-Id: Iddf52b7e75728d1aa1b43a832cc94e94b30b191d
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>

--- Android.mk
+++ Android.mk
@@ -13,6 +13,8 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(PRODUCT_OUT)/efi
 LOCAL_MODULE_STEM := $(LOCAL_MODULE)
 LOCAL_SRC_FILES := ../../prebuilts/tools/linux-$(arch_name)/uefi_shim/MokManager.efi
+LOCAL_CERTIFICATE := SBSIGN
+LOCAL_SBSIGN_CERTIFICATE := uefi_shim_key
 include $(BUILD_PREBUILT)
 
 MOKMANAGER_EFI := $(PRODUCT_OUT)/efi/MokManager.efi
@@ -24,7 +26,15 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(PRODUCT_OUT)/efi
 LOCAL_MODULE_STEM := $(LOCAL_MODULE)
 LOCAL_SRC_FILES := ../../prebuilts/tools/linux-$(arch_name)/uefi_shim/shim.efi
+LOCAL_CERTIFICATE := SBSIGN
+LOCAL_SBSIGN_CERTIFICATE := uefi_bios_db_key
+LOCAL_SBSIGN_BINARY_REPLACE_CERTIFICATE := device/intel/support/testkeys/shim/shim.crt:uefi_shim_key
 include $(BUILD_PREBUILT)
 
 UEFI_SHIM_EFI := $(PRODUCT_OUT)/efi/shim.efi
 
+# This will be packaged into the target file
+# so that the certificate in the shim can be
+# replace later.
+TARGET_FILES_PACKAGE_DISCARD_FILES += \
+	device/intel/support/testkeys/shim/shim.crt
