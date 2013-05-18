00b50b0f590b1dae7fe228f1016a734bb3a8d719
Radivoje <radivoje.jovanovic@intel.com>
2013-05-17 17:17:18 -0700

    sign MokManager with BIOS DB key
    Change-Id: I460fa560913edfc48de35daac224b86ee373684b
    Signed-off-by: Radivoje Jovanovic <radivoje.jovanovic@intel.com>

--- Android.mk
+++ Android.mk
@@ -14,7 +14,7 @@ LOCAL_MODULE_PATH := $(PRODUCT_OUT)/efi
 LOCAL_MODULE_STEM := $(LOCAL_MODULE)
 LOCAL_SRC_FILES := ../../prebuilts/tools/linux-$(arch_name)/uefi_shim/MokManager.efi
 LOCAL_CERTIFICATE := SBSIGN
-LOCAL_SBSIGN_CERTIFICATE := uefi_shim_key
+LOCAL_SBSIGN_CERTIFICATE := uefi_bios_db_key
 include $(BUILD_PREBUILT)
 
 MOKMANAGER_EFI := $(PRODUCT_OUT)/efi/MokManager.efi
